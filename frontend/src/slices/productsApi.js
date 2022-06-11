0257dc27af87a763d2f1e717e9fefba671074975
Leonardo Repolust <leonardo.repolust@gmail.com>
2022-06-11 11:30:56 +0200

    mixYourDrink Button

--- frontend/src/slices/productsApi.js
+++ frontend/src/slices/productsApi.js
@@ -3,13 +3,13 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
-  reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
-  endpoints: (builder) => ({
-    getAllProducts: builder.query({
-      query: () => `products`,
+    reducerPath: "productsApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/" }),
+    endpoints: (builder) => ({
+        getAllProducts: builder.query({
+            query: () => `products`,
+        }),
     }),
-  }),
 });
 
 // Export hooks for usage in functional components, which are
