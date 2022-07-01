a031746e9ae5f66cc36414e7f16a7696d2c1cbb8
David KUKOVIC <s49405@edu.campus02.at>
2022-07-01 14:58:53 +0200

    Feedback

--- frontend/src/components/Feedback.jsx
+++ frontend/src/components/Feedback.jsx
@@ -1,34 +1,109 @@
-import { useEffect, useState } from "react";
-import { ServoiceFeedback } from 'react-feedback-widget';
-
-
+import { useState } from "react";
 import 'react-responsive-modal/styles.css';
+import { Rating } from 'react-simple-star-rating';
+
+// import { ServoiceFeedback } from 'react-feedback-widget';
 
 
-const Feedback = () => {
+
+/* export default function Feedback() {
+  const [rating, setRating] = useState(100)
   
-  const [open, setOpen] = useState(false);
-
-  const config = {
-    servId: '1111111',
-    servPID: 'kjh34kl2jh4kj234',
-    userEmail: 'test@feedbackisgreat.com',
-    userFirstName: 'David',
-    userLastName: 'Groechel',
-  };
-
-  
+  const handleRating = (rate) => {
+    setRating(rate)
+  }
 
   return (
     <div className="cart-container">
       <h2>Feedback</h2>
-      <div className="App">
-      <ServoiceFeedback config={config}>
-        <button type="button">Feedback</button>
-      </ServoiceFeedback>
+      <Rating 
+        onClick={handleRating}
+        ratingValue={rating} 
+        transition
+      />  
     </div>
+  )
+}; */
+  
+
+
+const Feedback = () => {
+  const [open, setOpen] = useState(false);
+  const [rating, setRating] = useState(false)
+ const config = {
+  servId: '1111111',
+     servPID: 'kjh34kl2jh4kj234',
+  userEmail: 'test@feedbackisgreat.com',
+    userFirstName: 'David',
+   userLastName: 'Groechel',
+  };
+
+  const handleRating = (rate) => {
+    setRating(rate)
+  }
+ return (
+    <div className="cart-container">
+      <h2>Feedback</h2>
+      <Rating 
+        onClick={handleRating}
+        ratingValue={rating} 
+        transition
+      />  
     </div>
-  );
-};
+  )
+  
+}
 
 export default Feedback;
+
+
+// const Stars = () => {
+
+  
+//   // const [open, setOpen] = useState(false);
+
+//   // const config = {
+//   //   servId: '1111111',
+//   //   servPID: 'kjh34kl2jh4kj234',
+//   //   userEmail: 'test@feedbackisgreat.com',
+//   //   userFirstName: 'David',
+//   //   userLastName: 'Groechel',
+//   // };
+
+  
+//   // Catch Rating value
+
+
+//   return (
+//     <Rating 
+//       onClick={handleRating}
+//       ratingValue={rating} 
+//       transition
+//     />
+//   )
+// }
+
+// const Feedback = () => {
+//   return (
+//     <div className="cart-container">
+//       <h2>Feedback</h2>
+//       <Rating />
+//     </div>
+//   )
+// }
+
+//  export default Feedback
+
+
+
+
+ //      /* <ServoiceFeedback config={config}>
+//        <button type="button">Feedback</button>
+ //     </ServoiceFeedback> */
+/*  window.React1 = require('react');
+
+ console.log("here");
+// Add this in your component file
+require('react-dom');
+console.log(window.React1 === window.React2); */
+
